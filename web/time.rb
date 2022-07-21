84cb9139e64a21804b35c04c104d5bee37bb4fa2
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-07-21 14:00:28 +0100

    Add files via upload

---
+++ web/time.rb
@@ -0,0 +1,17 @@
+#!usr/bin/ruby -w
+
+time = Time.new
+
+# Components of a time
+
+puts "Current Time : " + time.inspect
+puts time.year
+puts time.month
+puts time.day
+puts time.wday
+puts time.yday
+puts time.hour
+puts time.min
+puts time.sec
+puts time.usec
+puts time.zone
